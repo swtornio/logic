72dd4d079f84ac7e81b09f7bd14de9ed740967ab
Steve Tornio <swtornio@gmail.com>
2023-04-27 08:26:39 -0500

    first commit

---
+++ logic.py
@@ -0,0 +1,5 @@
+# logic - april 27, 2023
+#
+# Program to take a list of email addresses consisting of letters and numbers
+# and report the most common base letters, and optionally output a new list
+# of email addresses with those base letters and an optional range of numbers.
